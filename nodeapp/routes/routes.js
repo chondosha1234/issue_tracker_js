9d94c5576fadf81d19ddc7cd365a0bd5925bb6a0
chondosha1234 <jmiller61193@gmail.com>
2022-02-08 14:45:41 -0500

    changed url paths and added some routes for finish and update issues

--- nodeapp/routes/routes.js
+++ nodeapp/routes/routes.js
@@ -12,16 +12,22 @@ router.get('/home/projects', page_controller.projects);
 router.get('/home/projects/create_project', page_controller.create_project);
 router.post('/home/projects', function_controller.createProject);
 router.get('/home/projects/project_detail/:id', page_controller.project_detail);
+router.get('/home/projects/finish_project', page_controller.finish_project);
+router.post('/home/projects/finish_project', function_controller.finishProject);
 
 router.get('/home/users', page_controller.users);
 router.get('/home/users/user_info/:username', page_controller.user_info);
 
 router.get('/home/issues', page_controller.issues);
+router.get('/home/issues/issue_detail', page_controller.issue_detail);
 router.get('/home/issues/create_issue', page_controller.create_issue);
-router.post('/home/issues', function_controller.createIssue);
+router.post('/home/issues/create_issue', function_controller.createIssue);
 router.get('/home/issues/assign_issue', page_controller.assign_issue);
 router.post('/home/issues/assign_issue', function_controller.assignIssue);    //check to see if works
-router.get('/home/issues/issue_detail', page_controller.issue_detail);
+router.get('/home/issues/update_issue', page_controller.update_issue);
+router.post('/home/issues/update_issue', function_controller.updateIssue);
+router.get('/home/issues/close_issue', page_controller.close_issue);
+router.post('/home/issues/close_issue', function_controller.closeIssue);
 router.get('/home/issues/overdue_issues', function_controller.showOverdueIssues);
 router.get('/home/issues/open_issues', function_controller.showOpenIssues);
 router.get('/home/issues/all_issues', function_controller.showAllIssues);
